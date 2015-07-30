98b995a18c2b6ebdb56594d0b5a0c5a1f4d8d4f5
tesyolan <tesfa1993@yahoo.com>
2015-07-30 06:19:56 +0300

    Fixed postion.

--- scripts/ros.js
+++ scripts/ros.js
@@ -141,7 +141,7 @@ function startROS() {
 		var id = gapi.hangout.getLocalParticipantId(); 
 		var canvas = gapi.hangout.layout.getVideoCanvas(); 
 		
-		canvas.style.postion ="absolute"; 
+		canvas.style.position ="absolute"; 
 		canvas.setWidth(400); 
 		canvas.setVisible(true); 
 		
