e5ac43bcd93941a1ab116b1c538c03943c8f1e1f
tesyolan <tesfa1993@yahoo.com>
2015-08-03 12:56:31 +0300

    First try at integrating AngularJS to the ROS api.

--- scripts/ros.js
+++ scripts/ros.js
@@ -32,125 +32,84 @@ function startROS() {
 	HangoutDemo.prototype.onApiReady = function (event) {	
 		if (event.isApiReady === true) {
 			
-			
-			loadCanvas();  		 
-			startROS();
-			
+			startROS(); 
 			console.log("API Ready");	
 			gapi.hangout.onParticipantsChanged.add(
 				this.onParticipantsChanged.bind(this)); 
-				//removed the text id. 
-				document.getElementById('input').onkeypress= this.pressedEnter.bind(this); 
-		//document.getElementById("sendMessage").onclick = 	// callback for button-click
-          //this.buttonClick.bind(this);
+
         		this.displayParticipants();
 	
 		}	
 		};  
-	HangoutDemo.prototype.pressedEnter= function (e) {
-				var keycode = e.keyCode; 
-					if(keycode==13) {
-						var tag= document.createElement('div'); 
-						tag.className="numberCircle"; 
-						
-						var newDiv= document.createElement('div'); 
-						newDiv.className="chatparticipant"; 
-						var div2= document.getElementById("chat"); 
-						var participant= gapi.hangout.getLocalParticipant().person.displayName;
-						var space=participant.trim().indexOf(" "); 
-						if(space==-1)
-						{
-							tag.innerHTML=participant.charAt(0); 
-						}
-						else 
-						{
-							//Currently Represents Names as a form of Two latters only
-							tag.innerHTML= participant.charAt(0) + participant.charAt(space+1); 
-							
-						}
-						div2.appendChild(tag); 
-						div2.appendChild(newDiv); 
-						//Now this is where the condition
-						newDiv.innerHTML= document.getElementById("input").innerHTML;
-						message.utterance = document.getElementById("input").innerHTML; 
-						chatTopic.publish(message); 
-						
-						
-						document.getElementById("input").innerHTML=""; 
-						
-						//Also this is where the message would necessaryly be sent. 
-						//Asso where the processing of the partificipants name be filtered. 
-					}
-	}
-	//HangoutDemo.prototype.buttonClick = function () {
-	
-		
-     //var newDiv= document.createElement('div'); 
-     //newDiv.className="chatparticipant"; 
-     //var div2= document.getElementById("chat"); 
-     //div2.appendChild(newDiv); 
-     
-     //newDiv.innerHTML= document.getElementById("input").innerHTML;
-     //document.getElementById("input").innerHTML="Enter the next Question for Eva"; 
-      
-      ////var value = gapi.hangout.data.getValue("count") || "0";	// read current count from state
-      ////value = (parseInt(value, 10) + 1).toString();	// increment count by one
-     //// gapi.hangout.data.setValue("count", value);	// write new count into state
 
-    //};
+
   HangoutDemo.prototype.onParticipantsChanged = function (event) {	
 
-      var div = document.getElementById("container");	
-
-      div.innerHTML = "";	// make sure our container is empty before displaying the list
-
-      this.displayParticipants();	
 
     };
   
   HangoutDemo.prototype.displayParticipants = function () {	
-      var div, participants, ul, li, i, l;	
-      div = document.getElementById("available");	
-      participants = gapi.hangout.getParticipants();	// Get array of participants from API
-      ul = document.createElement("ul");	
-      l = participants.length;
-	console.log("The length is : "); 
-	console.log(l);	
-	participants_list=[]; 
-	
-	if(rosok)
-	{
-	console.log("ROS Conneciton is verified. "); 
-	li = document.createElement("li"); 
-	li.innerHTML= "Sophia";
-	ul.appendChild(li); 
-    div.appendChild(ul);	
-	participants_list.push("Sophia");  
-	}
-      for (i = 0; i < l; i++) {	
-        li = document.createElement("li");	
-        if (participants[i].person) {	
-          li.innerHTML = participants[i].person.displayName;	// Add name to list if available
-        } else {	
-          li.innerHTML = "unknown";	
-        }	
-        ul.appendChild(li);	
-        participants_list.push(li.innerHTML); 
-      }	
-      
-      div.appendChild(ul);	
+
     };  
-    function loadCanvas() {
-		var id = gapi.hangout.getLocalParticipantId(); 
-		var canvas = gapi.hangout.layout.getVideoCanvas(); 
-		
-		//canvas.style.position ="absolute"; 
-		canvas.setWidth(400); 
-		canvas.setVisible(true); 
-		
-		var right= document.getElementById("sophia"); 
-		right.appendChild(canvas); 
-	}
+    
+    function NgChatCtrl($scope) {
+            // Our server to connect to
+  		
+            //var listenMessage = new ROSLIB.Message({
+            //response : ''
+            //}); 
+
+			//chatResponse.subscribe(function(message) {
+			//listenMessage.response = message.data; 
+
+			//console.log(message.data); 
+			//}); 
+			
+			
+			var side = 'left';
+
+            // Messages, client info & sending
+            $scope.messages = [];
+            $scope.sendMessage = function () {
+                //server.sendNgChatMessage($scope.messageText);
+                message.utterance  = $scope.messageText; 
+                chatTopic.publish(message); 
+                console.log(message.utterance); 
+                chatPublish(message.utterance); 
+                
+                $scope.messageText = "";
+            };
+            // Occurs when we receive chat messages
+            //server.on('ngChatMessagesInform', function (p) {
+			chatResponse.subscribe(function(p) {
+				console.log(p.data); 
+				chatPublish(p.data); 
+              
+            });
+            function chatPublish(p) {
+				
+			$scope.messages.push({
+                    //avatar: "data:image/png;base64," + p.avatar.toBase64(),
+                    
+                    text: p,
+                    side: side
+				}); 
+            		
+
+                // Animate
+                $("#viewport-content").animate({
+                    bottom: $("#viewport-content").height() - $("#viewport").height()
+                }, 250);
+
+                // flip the side
+                side = side == 'left' ? 'right' : 'left';
+			}
+			
+			//Now here we take out pulisher to handle when the user passes data to ROS and when the User pulishes the message. 
+            // Once connected, we need to join the chat
+
+        }
+
 		var ros; 
         var hangoutDemo= new HangoutDemo(); 
         var rosok=false;  
@@ -176,11 +135,6 @@ function startROS() {
 		response : ''
 	}); 
 	//Call back fucntion for the audio tracker. 
-	chatResponse.subscribe(function(message) {
-		listenMessage.response = message.data; 
-		//Here Pulish the topic to a new div in topic. 
-		console.log(message.data); 
-	}); 
-	
+
 	
 }(window)); 
