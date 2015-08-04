a1193f61789d6bcd1f2c780579782fa77fb156a6
tesyolan <tesfa1993@yahoo.com>
2015-08-04 16:22:31 +0300

    Refactoring the angular js file out of the system.

--- scripts/ros.js
+++ scripts/ros.js
@@ -1,3 +1,86 @@
+        
+        
+	var ros; 
+	var rosok=false;  
+	var participants_list;    
+//===========================================================================
+	var chatTopic = new ROSLIB.Topic({
+		ros : ros, 
+		name : '/eva/chatbot_speech',
+		messageType : 'chatbot/ChatMessage'
+	}); 
+	var message = new ROSLIB.Message({
+		utterance : '', 
+		confidence : 90
+		}); 
+//===========================================================================		
+	var chatResponse = new ROSLIB.Topic({
+		ros : ros, 
+		name : '/eva/chatbot_responses',
+		messageType : 'std_msgs/String'
+	}); 
+	
+	var listenMessage = new ROSLIB.Message({
+		response : ''
+	}); 
+  
+  function NgChatCtrl($scope) {
+            // Our server to connect to
+  		
+            //var listenMessage = new ROSLIB.Message({
+            //response : ''
+            //}); 
+
+			//chatResponse.subscribe(function(message) {
+			//listenMessage.response = message.data; 
+
+			//console.log(message.data); 
+			//}); 
+			
+			
+			var side = 'left';
+
+            // Messages, client info & sending
+            $scope.messages = [];
+            $scope.sendMessage = function () {
+                //server.sendNgChatMessage($scope.messageText);
+                message.utterance  = $scope.messageText; 
+                chatTopic.publish(message); 
+                console.log(message.utterance); 
+                chatPublish(message.utterance); 
+                
+                $scope.messageText = "";
+            };
+            // Occurs when we receive chat messages
+            //server.on('ngChatMessagesInform', function (p) {
+			chatResponse.subscribe(function(p) {
+				console.log(p.data); 
+				chatPublish(p.data); 
+              
+            });
+            function chatPublish(p) {
+				
+			$scope.messages.push({
+                    //avatar: "data:image/png;base64," + p.avatar.toBase64(),
+                    text: p,
+                    side: side
+				}); 
+            		
+
+                // Animate
+                $("#viewport-content").animate({
+                    bottom: $("#viewport-content").height() - $("#viewport").height()
+                }, 250);
+
+                // flip the side
+                side = side == 'left' ? 'right' : 'left';
+			}
+			
+			//Now here we take out pulisher to handle when the user passes data to ROS and when the User pulishes the message. 
+            // Once connected, we need to join the chat
+
+        }
+ 
  (function (window) {
         "use strict"; 
         function HangoutDemo() {
@@ -6,9 +89,8 @@
         gapi.hangout.onApiReady.add(this.onApiReady.bind(this)); 
         
         }
-        
- 
-function startROS() {
+	
+	function startROS() {
 	ros = new ROSLIB.Ros ({ 
 	  url : 'wss://localhost:9094'
 	  }); 
@@ -54,30 +136,9 @@ function startROS() {
     
     
 
-		var ros; 
+
         var hangoutDemo= new HangoutDemo(); 
-        var rosok=false;  
-		var participants_list;    
-//===========================================================================
-	var chatTopic = new ROSLIB.Topic({
-		ros : ros, 
-		name : '/eva/chatbot_speech',
-		messageType : 'chatbot/ChatMessage'
-	}); 
-	var message = new ROSLIB.Message({
-		utterance : '', 
-		confidence : 90
-		}); 
-//===========================================================================		
-	var chatResponse = new ROSLIB.Topic({
-		ros : ros, 
-		name : '/eva/chatbot_responses',
-		messageType : 'std_msgs/String'
-	}); 
-	
-	var listenMessage = new ROSLIB.Message({
-		response : ''
-	}); 
+
 	//Call back fucntion for the audio tracker. 
 
 	
